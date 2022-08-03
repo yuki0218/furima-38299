18700726c07f82f57649ddaf989a7b2f6362cb44
yuki0218 <ktnyk0305@gmail.com>
2022-08-03 16:55:24 +0900

    itemのセレクターの処理

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,13 +1,19 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :delivery_charge
+  belongs_to :prefecture
+  belongs_to :send_days
   belongs_to :user
   has_one_attached :image
 
   validates :name, presence: true
   validates :description, presence: true
-  validates :category_id, presence: true
-  validates :condition_id, presence: true
-  validates :delivery_charge_id, presence: true
-  validates :prefecture_id, presence: true
-  validates :send_days_id, presence: true
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank"}
+  validates :condition_id, numericality: { other_than: 1, message: "can't be blank"}
+  validates :delivery_charge_id, numericality: { other_than: 1, message: "can't be blank"}
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank"}
+  validates :send_days_id, numericality: { other_than: 1, message: "can't be blank"}
   validates :price, presence: true
 end
