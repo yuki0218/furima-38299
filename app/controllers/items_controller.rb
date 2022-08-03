8a5b8c5f1b4e390c2bf8889ef6629ff1fef0cac0
yuki0218 <ktnyk0305@gmail.com>
2022-08-03 15:29:14 +0900

    itemの画像の処理

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,4 +7,9 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
+
+  private
+  def item_params
+    params.require(:item).permit(:image, :name, :description, :category_id, :condition_id, :delivery_charge_id, :prefecture_id, :send_days_id, :price).merge(user_id: current_user.id)
+  end
 end
