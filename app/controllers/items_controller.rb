63b2aef134ae25a45a63dca22c4d12ee665deb2d
yuki0218 <ktnyk0305@gmail.com>
2022-08-08 14:23:59 +0900

    削除ボタンの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -34,6 +34,12 @@ class ItemsController < ApplicationController
     end
   end
 
+  def destroy
+    item = Item.find(params[:id])
+    item.destroy
+    redirect_to root_path
+  end
+
   private
   def item_params
     params.require(:item).permit(:image, :name, :description, :category_id, :condition_id, :delivery_charge_id, :prefecture_id, :send_days_id, :price).merge(user_id: current_user.id)
