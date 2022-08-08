054c3a2fd3a378145ceb331462d9cc27ba9c90e2
yuki0218 <ktnyk0305@gmail.com>
2022-08-08 14:32:48 +0900

    destroyアクションへの制限

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :edit]
-  before_action :move_to_index, only: :edit
+  before_action :authenticate_user!, only: [:new, :edit, :destroy]
+  before_action :move_to_index, only: [:edit, :destroy]
   before_action :find_item, only: [:show, :edit, :update]
 
   def index
