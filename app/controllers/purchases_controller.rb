b51e4184010b6774e408dccbdfeb0bf12e34be8b
yuki0218 <ktnyk0305@gmail.com>
2022-08-09 18:09:34 +0900

    PAY.JPの導入

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -5,18 +5,28 @@ class PurchasesController < ApplicationController
   end
 
   def create
+    @item = Item.find(params[:item_id])
     @address_record = AddressRecord.new(purchase_params)
     if @address_record.valid?
+      pay_item
       @address_record.save
       redirect_to root_path
     else
-      @item = Item.find(params[:item_id])
       render :index
     end
   end
 
   private
   def purchase_params
-    params.require(:address_record).permit(:post_code, :prefecture_id, :city, :address, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:address_record).permit(:post_code, :prefecture_id, :city, :address, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: purchase_params[:token],
+      currency: 'jpy'
+    )
   end
 end
--- app/models/address_record.rb
+++ app/models/address_record.rb
@@ -1,6 +1,6 @@
 class AddressRecord
   include ActiveModel::Model
-  attr_accessor :post_code, :prefecture_id, :city, :address, :building, :phone_number, :user_id, :item_id
+  attr_accessor :post_code, :prefecture_id, :city, :address, :building, :phone_number, :user_id, :item_id, :token
 
   with_options presence: true do
     validates :post_code, format: {with: /\A\d{3}-\d{4}\z/}
@@ -9,6 +9,7 @@ class AddressRecord
     validates :phone_number, format: {with: /\A\d{10,11}\z/}
     validates :user_id
     validates :item_id
+    validates :token
   end
   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank"}
 
