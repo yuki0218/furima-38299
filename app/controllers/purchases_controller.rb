fc9e2fde6d5eac4123b2c5175069bfda0a75a0ac
yuki0218 <ktnyk0305@gmail.com>
2022-08-09 11:19:57 +0900

    purchases#createの実装

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,5 +1,21 @@
 class PurchasesController < ApplicationController
   def index
     @item = Item.find(params[:item_id])
+    @address_record = AddressRecord.new
+  end
+
+  def create
+    @address_record = AddressRecord.new(purchase_params)
+    if @address_record.valid?
+      @address_record.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+  def purchase_params
+    params.require(:address_record).permit(:post_code, :prefecture_id, :city, :address, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 end
